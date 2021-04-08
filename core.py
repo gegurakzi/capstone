e85639485226980c31e551d4104826a2036d751b
호형 강 <fourierk@naver.com>
2021-04-08 12:13:43 +0900

    punctuator 추가 / readability 계산 로직 수정

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
+import json
 from word_analyzer import WordAnalyzer
 from script_analyzer import ScriptAnalyzer
-import json
+from punctuator import Punctuator
 
 
 target_video_id = "TLnUJzueBOQ"
@@ -10,20 +11,29 @@ bbc_news = 'hFAROEKiHl8'
 
 WA = WordAnalyzer()
 SA = ScriptAnalyzer()
+# Place model files at '~/.punctuator' Download ULR : https://drive.google.com/drive/folders/0B7BsN5f2F1fZQnFsbzJ3TWxxMms
+# model list : Demo-Europarl-EN.pcl INTERSPEECH-T-BRNN-pre.pcl INTERSPEECH-T-BRNN.pcl
+P = Punctuator('Demo-Europarl-EN.pcl')
 
 
 def analyzeAll(videoId):
     sa_result = json.loads(SA.analyzeScript(videoId))
-    wa_result = json.loads(WA.analyzeText(sa_result['script']))
+    print('script analyze ok')
+    punc_script = P.punctuate(sa_result['script'])  # 문장부호 포함된 스크립트
+    print('punctuator ok')
+    wa_result = json.loads(WA.analyzeText(punc_script))
+    print('word analyze ok')
 
     analyze_result = {}
 
     analyze_result['videoId'] = sa_result['videoId']
-    analyze_result['script'] = sa_result['script']
-    analyze_result['avgSyllPerSec'] = sa_result['avgSyllPerSec']
+    analyze_result['script'] = punc_script
     analyze_result['totalWords'] = wa_result['Total_words']
+    analyze_result['totalUniqueWords'] = wa_result['Total_unique_words']
+    analyze_result['totalSentences'] = wa_result['Total_sentences']
+    analyze_result['avgSyllPerSec'] = sa_result['avgSyllPerSec']
     analyze_result['avgCEFRScore'] = wa_result['Total_avg_CEFR']
-    analyze_result['Readability'] = wa_result['DC_Readability']
+    analyze_result['readability'] = wa_result['DC_Readability']
     analyze_result['uncommonRatio'] = wa_result['DCL']['uncommon_ratio']
 
     cefr_sum = [0, 0, 0, 0, 0, 0, 0]
@@ -34,7 +44,7 @@ def analyzeAll(videoId):
                     cefr_sum[idx] += len(wa_result[checker]
                                          [cefr]['classified_words'][level])
     cefr_ratio = list(
-        map(lambda x: (x/5)/analyze_result['totalWords']*100, cefr_sum))
+        map(lambda x: (x/5)/analyze_result['totalUniqueWords']*100, cefr_sum))
 
     analyze_result['A1ratio'] = cefr_ratio[0]
     analyze_result['A2ratio'] = cefr_ratio[1]
@@ -44,10 +54,12 @@ def analyzeAll(videoId):
     analyze_result['C2ratio'] = cefr_ratio[5]
     analyze_result['Nratio'] = cefr_ratio[6]
 
+    print('analyze ok')
+
     return json.dumps(analyze_result, indent=4)
 
 
-output = analyzeAll(bbc_news)
+output = analyzeAll(comedy_central)
 
-with open('bbcnews_test.json', 'wt', encoding='UTF-8') as f:
+with open('comedycentral_test.json', 'wt', encoding='UTF-8') as f:
     f.write(output)
--- word_analyzer.py
+++ word_analyzer.py
@@ -19,43 +19,52 @@ stem = PorterStemmer()
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 
-pos_map = defaultdict(lambda : wn.NOUN)
+pos_map = defaultdict(lambda: wn.NOUN)
 pos_map['J'] = wn.ADJ
 pos_map['V'] = wn.VERB
 pos_map['R'] = wn.ADV
 
 
 class WordAnalyzer:
-    Dale_Chall_List = pd.read_csv("./word_data/Dale_Chall_List.csv",index_col=0).values.tolist()
-    CEFR2score = {'A1':1, 'A2':2, 'B1':3, 'B2':4, 'C1':5, 'C2':6, 'N':6}
-    oxford_CEFR = pd.read_csv('./word_data/oxford_5000_CEFR.csv',index_col=0).values.tolist()
-    japanese_CEFR = pd.read_csv('./word_data/cefr_japanese.csv',index_col=0).values.tolist()
-    pg_freq_10000 = pd.read_csv('./word_data/project_gutenberg_10000.csv',index_col=0).values.tolist()
-    simpson_freq_5000 = pd.read_csv('./word_data/simpson_freq_5000.csv',index_col=0).values.tolist()
-    tv_freq_10000 = pd.read_csv('./word_data/tv_movie_script_10000.csv',index_col=0).values.tolist()
+    Dale_Chall_List = pd.read_csv(
+        "./word_data/Dale_Chall_List.csv", index_col=0).values.tolist()
+    CEFR2score = {'A1': 1, 'A2': 2, 'B1': 3, 'B2': 4, 'C1': 5, 'C2': 6, 'N': 6}
+    oxford_CEFR = pd.read_csv(
+        './word_data/oxford_5000_CEFR.csv', index_col=0).values.tolist()
+    japanese_CEFR = pd.read_csv(
+        './word_data/cefr_japanese.csv', index_col=0).values.tolist()
+    pg_freq_10000 = pd.read_csv(
+        './word_data/project_gutenberg_10000.csv', index_col=0).values.tolist()
+    simpson_freq_5000 = pd.read_csv(
+        './word_data/simpson_freq_5000.csv', index_col=0).values.tolist()
+    tv_freq_10000 = pd.read_csv(
+        './word_data/tv_movie_script_10000.csv', index_col=0).values.tolist()
 
     ###### 전처리부 #########
-    #텍스트 전처리- 영어 이외에 전부 공백처리, 전부 소문자화
+    # 텍스트 전처리- 영어 이외에 전부 공백처리, 전부 소문자화
     def preprocessing(self, text):
         clean_text = re.sub(pattern='[^a-zA-Z]', repl=' ', string=text)
         return clean_text
-    #텍스트를 단어 리스트로 변환(stopword 적용)
+    # 텍스트를 단어 리스트로 변환(stopword 적용)
+
     def text2list(self, text):
-        return list(set([word for word in text.lower().split() if not word in set(stopwords.words('english'))]))
-    #stemming , 어간 추출을 통해 단어를 원형으로 복원한다.
+        return [word for word in text.lower().split() if not word in set(stopwords.words('english'))]
+    # stemming , 어간 추출을 통해 단어를 원형으로 복원한다.
+
     def stemming(self, text2list):
         tag_list = pos_tag(text2list)
         # return list(map(lambda x : stem.stem(x),text2list))
-        return list(map(lambda x : lemma.lemmatize(x[0] , pos_map[x[1][0]]),tag_list))  
+        return list(map(lambda x: lemma.lemmatize(x[0], pos_map[x[1][0]]), tag_list))
 
     ###### DCL 부 ############
     # Dale Chall List 에 기반한 uncommon word 확인(리스트에 없으면 uncommon)
     def checkDCL(self, word):
-        if word in map(lambda x: x[0],self.Dale_Chall_List):
-            return False #common
+        if word in map(lambda x: x[0], self.Dale_Chall_List):
+            return False  # common
         else:
-            return True #uncommon
+            return True  # uncommon
     # uncommon 단어 추출
+
     def extractUncommon(self, text2list):
         uncommon_list = []
         for word in text2list:
@@ -71,14 +80,16 @@ class WordAnalyzer:
                 return item[1]
         return 'N'
     # 난이도별 단어 추출
+
     def extractCEFR(self, text2list, CEFR=oxford_CEFR):
-        CEFR_words = {'A1':[],'A2':[], 'B1':[],'B2':[],'C1':[],'C2':[], 'N':[]}
+        CEFR_words = {'A1': [], 'A2': [], 'B1': [],
+                      'B2': [], 'C1': [], 'C2': [], 'N': []}
         for word in text2list:
-            CEFR_words[self.checkCEFR(word,CEFR)].append(word)
+            CEFR_words[self.checkCEFR(word, CEFR)].append(word)
         return CEFR_words
 
     ########### freq 부 ###########
-    #freq 부는 stemming을 하지 않은 텍스트로 적용해야함
+    # freq 부는 stemming을 하지 않은 텍스트로 적용해야함
     # 빈도수 랭킹을 반환 (없으면 -1)
     def checkFreq(self, word, Freq=tv_freq_10000):
         for item in Freq:
@@ -86,8 +97,10 @@ class WordAnalyzer:
                 return item[0]
         return -1
     # 빈도수 랭킹을 cefr 점수로 변환
+
     def extractFreq(self, text2list, Freq=tv_freq_10000):
-        CEFR_words = {'A1':[],'A2':[], 'B1':[],'B2':[],'C1':[],'C2':[], 'N':[]}
+        CEFR_words = {'A1': [], 'A2': [], 'B1': [],
+                      'B2': [], 'C1': [], 'C2': [], 'N': []}
         for word in text2list:
             if self.checkFreq(word, Freq) == -1:
                 CEFR_words['N'].append(word)
@@ -105,9 +118,9 @@ class WordAnalyzer:
                 CEFR_words['C2'].append(word)
         return CEFR_words
 
-    
     ####### 최종 아웃풋 ########
     # cefr 점수 평균 계산
+
     def calAvgCEFR(self, classified_words, total_words):
         score_sum = 0
         CEFR_level = classified_words.keys()
@@ -115,71 +128,85 @@ class WordAnalyzer:
             score_sum += len(classified_words[level])*self.CEFR2score[level]
         return score_sum/total_words
     # Dale Chall 가독성 점수 계산
+
     def calReadability(self, uncommon_ratio, total_words, total_sentences):
         return 0.1579*uncommon_ratio + 0.0496*(total_words/total_sentences)
 
-
     def analyzeText(self, text):
         output_json = {
-            'Input_text' : '',
-            'Total_words' : 0,
-            'Total_sentences' : 0,
-            'Total_avg_CEFR' : 0,
-            'DC_Readability' : 0,
-            'DCL' : {
-                'uncommon_ratio' : 0,
-                'uncommon_words' : [],
+            'Input_text': '',
+            'Total_words': 0,
+            'Total_sentences': 0,
+            'Total_avg_CEFR': 0,
+            'DC_Readability': 0,
+            'DCL': {
+                'uncommon_ratio': 0,
+                'uncommon_words': [],
             },
-            'CEFR' : {
-                'Oxford' : {
-                    'avg_CEFR' : 0,
-                    'classified_words' : [],
-                },
-                'Japanese' : {
-                    'avg_CEFR' : 0,
-                    'classified_words' : [],
-                }
-            },
-            'Freq' : {
-                'Tv' : {
-                    'avg_CEFR' : 0,
+            'CEFR': {
+                'Oxford': {
+                    'avg_CEFR': 0,
                     'classified_words': [],
                 },
-                'Simpson' : {
-                    'avg_CEFR' : 0,
-                    'classified_words' : [],
-                },
-                'Gutenberg' : {
+                'Japanese': {
                     'avg_CEFR': 0,
-                    'classified_words' : []
+                    'classified_words': [],
+                }
+            },
+            'Freq': {
+                'Tv': {
+                    'avg_CEFR': 0,
+                    'classified_words': [],
+                },
+                'Simpson': {
+                    'avg_CEFR': 0,
+                    'classified_words': [],
+                },
+                'Gutenberg': {
+                    'avg_CEFR': 0,
+                    'classified_words': []
                 }
             }
         }
         output_json['Input_text'] = text
         clean_text = self.preprocessing(text)
         word_list = self.text2list(clean_text)
-        word_list_stem = self.stemming(word_list)
+        word_list_stem = list(set(self.stemming(word_list)))
         total_words = len(word_list)
+        total_unique_words = len(word_list_stem)
         output_json['Total_words'] = total_words
+        output_json['Total_unique_words'] = total_unique_words
         output_json['Total_sentences'] = len(sent_tokenize(text))
 
-        output_json['DCL']['uncommon_words'] = self.extractUncommon(word_list_stem)
-        output_json['DCL']['uncommon_ratio'] = len(output_json['DCL']['uncommon_words'])/total_words*100
-        output_json['DC_Readability'] = self.calReadability(output_json['DCL']['uncommon_ratio'],output_json['Total_words'], output_json['Total_sentences'])
+        output_json['DCL']['uncommon_words'] = self.extractUncommon(
+            word_list_stem)
+        output_json['DCL']['uncommon_ratio'] = len(
+            output_json['DCL']['uncommon_words'])/total_unique_words*100
+        output_json['DC_Readability'] = self.calReadability(
+            output_json['DCL']['uncommon_ratio'], total_words, output_json['Total_sentences'])
 
-        output_json['CEFR']['Oxford']['classified_words'] = self.extractCEFR(word_list_stem,self.oxford_CEFR)
-        output_json['CEFR']['Oxford']['avg_CEFR'] = self.calAvgCEFR(output_json['CEFR']['Oxford']['classified_words'], total_words)
-        output_json['CEFR']['Japanese']['classified_words'] = self.extractCEFR(word_list_stem,self.japanese_CEFR)
-        output_json['CEFR']['Japanese']['avg_CEFR'] = self.calAvgCEFR(output_json['CEFR']['Japanese']['classified_words'], total_words)
+        output_json['CEFR']['Oxford']['classified_words'] = self.extractCEFR(
+            word_list_stem, self.oxford_CEFR)
+        output_json['CEFR']['Oxford']['avg_CEFR'] = self.calAvgCEFR(
+            output_json['CEFR']['Oxford']['classified_words'], total_unique_words)
+        output_json['CEFR']['Japanese']['classified_words'] = self.extractCEFR(
+            word_list_stem, self.japanese_CEFR)
+        output_json['CEFR']['Japanese']['avg_CEFR'] = self.calAvgCEFR(
+            output_json['CEFR']['Japanese']['classified_words'], total_unique_words)
 
-        output_json['Freq']['Tv']['classified_words'] = self.extractFreq(word_list_stem,self.tv_freq_10000)
-        output_json['Freq']['Tv']['avg_CEFR'] = self.calAvgCEFR(output_json['Freq']['Tv']['classified_words'], total_words)
-        output_json['Freq']['Simpson']['classified_words'] = self.extractFreq(word_list_stem,self.simpson_freq_5000)
-        output_json['Freq']['Simpson']['avg_CEFR'] = self.calAvgCEFR(output_json['Freq']['Simpson']['classified_words'], total_words)
-        output_json['Freq']['Gutenberg']['classified_words'] = self.extractFreq(word_list_stem,self.pg_freq_10000)
-        output_json['Freq']['Gutenberg']['avg_CEFR'] = self.calAvgCEFR(output_json['Freq']['Gutenberg']['classified_words'], total_words)
+        output_json['Freq']['Tv']['classified_words'] = self.extractFreq(
+            word_list_stem, self.tv_freq_10000)
+        output_json['Freq']['Tv']['avg_CEFR'] = self.calAvgCEFR(
+            output_json['Freq']['Tv']['classified_words'], total_unique_words)
+        output_json['Freq']['Simpson']['classified_words'] = self.extractFreq(
+            word_list_stem, self.simpson_freq_5000)
+        output_json['Freq']['Simpson']['avg_CEFR'] = self.calAvgCEFR(
+            output_json['Freq']['Simpson']['classified_words'], total_unique_words)
+        output_json['Freq']['Gutenberg']['classified_words'] = self.extractFreq(
+            word_list_stem, self.pg_freq_10000)
+        output_json['Freq']['Gutenberg']['avg_CEFR'] = self.calAvgCEFR(
+            output_json['Freq']['Gutenberg']['classified_words'], total_unique_words)
 
-        output_json['Total_avg_CEFR'] = (output_json['CEFR']['Oxford']['avg_CEFR']+output_json['CEFR']['Japanese']['avg_CEFR']+output_json['Freq']['Tv']['avg_CEFR']+output_json['Freq']['Simpson']['avg_CEFR']+output_json['Freq']['Gutenberg']['avg_CEFR']) / 5
+        output_json['Total_avg_CEFR'] = (output_json['CEFR']['Oxford']['avg_CEFR']+output_json['CEFR']['Japanese']['avg_CEFR']+output_json['Freq']
+                                         ['Tv']['avg_CEFR']+output_json['Freq']['Simpson']['avg_CEFR']+output_json['Freq']['Gutenberg']['avg_CEFR']) / 5
         return json.dumps(output_json, indent=4)
-
-
